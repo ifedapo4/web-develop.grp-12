6b307edac44bb486fafacb81345c6577614fc027
Zaynab Odusote <113028028+ifedapo4@users.noreply.github.com>
2022-10-02 04:43:26 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const pageProgressBar = document.querySelector(".progress-bar")
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+  
+  pageProgressBar.style.width = `${scrolledPercentage}%`
+  
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const scrollTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
